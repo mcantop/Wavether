92d9c7ea00d08175d36b449b993cd8896ee9b274
mcantop <maciejcantop@icloud.com>
2022-09-13 22:48:33 +0200

    Initial Commit

---
+++ Wavether/WavetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WavetherApp.swift
+//  Wavether
+//
+//  Created by Maciej on 13/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct WavetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
